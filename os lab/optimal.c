b3fa824c46b5165eeda389633cb460bfc6ab0157
Mohammeduzair23 <mohammeduzair2k5@gmail.com>
2025-12-29 19:24:42 +0530

    Refactor optimal page replacement algorithm

--- os lab/optimal.c
+++ os lab/optimal.c
@@ -1,232 +1,97 @@
-/*#include<stdio.h>   
-int main() 
-{
-int i, j, k, l, m, n, p, c=0, s;
-int a[20], b[20], q, max;
-printf("Enter the number of references string: ");
-scanf("%d", &n);
-printf("Enter the size of the frame: ");
-scanf("%d", &m);
-printf("Enter the elements of the references string: \n");
-for(i=0; i<n; i++)
-scanf("%d", &a[i]);
-for(j=0; j<m; j++)
-b[j] = -1;
-for(i=0; i<n; i++){
-for(k=0; k<m; k++)
-if(b[k] == a[i])
-for(i=0; i<m; i++){                                     $$$  DOWN IS CORRECT CODE   $$$
-if(b[j] == -1) {
-b[j] = a[i];
-c++;
-}
-}
-if(j == m) {
-l=i+1, max=0;
-for(j=0; j<m; j++) {
-for(s=1; s<n; s++) {
-if(a[s] == b[j]) {
-if(s > max)
-{
-max = s;
-p = j;
-}
-break;
-}
-if(s == n) {
-max=s;
-p=j;
-}
-}
-}
-b[p] = a[i];
-c++;
-printf("\n\n");
-for(k=0; k<m; k++)
-printf("\n number of page fault is: %d", c);
-return 0;
-}
-}
-}
-*/
-
-/*
 #include <stdio.h>
 
-int main() {
-    int n, m;
-    int a[20], b[20];
-    int i, j, k, s;
-    int faults = 0;
+int main()
+{
+    int pages[50], frame[10], time[10];
+    int n, f, i, j, k;
+    int pageFaults = 0, found, least;
 
-    printf("Enter the number of reference strings: ");
+    printf("Enter number of pages: ");
     scanf("%d", &n);
 
-    printf("Enter the size of the frame: ");
-    scanf("%d", &m);
-
-    printf("Enter the reference string elements:\n");
+    printf("Enter page reference string: ");
     for (i = 0; i < n; i++)
-        scanf("%d", &a[i]);
+        scanf("%d", &pages[i]);
 
-    // Initialize frames
-    for (j = 0; j < m; j++)
-        b[j] = -1;
+    printf("Enter number of frames: ");
+    scanf("%d", &f);
 
-    // Process each reference
-    for (i = 0; i < n; i++) {
-        int hit = 0;
-
-        // Check if page already in frame
-        for (j = 0; j < m; j++) {
-            if (b[j] == a[i]) {
-                hit = 1;
-                break;
-            }
-        }
-
-        if (!hit) {
-            int replace = -1, farthest = -1;
-
-            // Look for an empty frame
-            for (j = 0; j < m; j++) {
-                if (b[j] == -1) {
-                    replace = j;
-                    break;
-                }
-            }
-
-            // If no empty frame → apply optimal replacement
-            if (replace == -1) {
-                for (j = 0; j < m; j++) {
-                    int nextUse = -1;
-
-                    for (s = i + 1; s < n; s++) {
-                        if (a[s] == b[j]) {
-                            nextUse = s;
-                            break;
-                        }
-                    }
-
-                    if (nextUse == -1) {   // Not used again → best to replace
-                        replace = j;
-                        break;
-                    }
-
-                    if (nextUse > farthest) {
-                        farthest = nextUse;
-                        replace = j;
-                    }
-                }
-            }
-
-            b[replace] = a[i];
-            faults++;
-        }
-
-        // Print frames
-        printf("After reference %d: ", a[i]);
-        for (k = 0; k < m; k++)
-            printf("%d ", b[k]);
-        printf("\n");
+    // Initialize frames and time
+    for (i = 0; i < f; i++)
+    {
+        frame[i] = -1;
+        time[i] = 0;
     }
 
-    printf("\nTotal page faults = %d\n", faults);
-
-    return 0;
-}
-*/
-
-
-#include<stdio.h>
-
-int main() 
-{
-    int i, j, k, l, m, n, p, c = 0, s;
-    int a[20], b[20], q, max;
-
-    printf("Enter the number of references string: ");
-    scanf("%d", &n);
-
-    printf("Enter the size of the frame: ");
-    scanf("%d", &m);
-
-    printf("Enter the elements of the references string:\n");
-    for (i = 0; i < n; i++)
-        scanf("%d", &a[i]);
-
-    for (j = 0; j < m; j++)
-        b[j] = -1;
+    printf("\nPage\tFrames\n");
 
     for (i = 0; i < n; i++)
     {
-        int found = 0;
+        found = 0;
 
-        // check hit
-        for (k = 0; k < m; k++)
+        // Check if page is in frame
+        for (j = 0; j < f; j++)
         {
-            if (b[k] == a[i])
+            if (frame[j] == pages[i])
             {
                 found = 1;
+                time[j] = i + 1;   // update recent use
                 break;
             }
         }
-
-        // if miss
-        if (!found)
+        // Page fault for FIFO Algorithm 
+        /* if (found == 0)
+           {
+              frames[k] = pages[i];
+              k = (k+1) % f;
+              pageFaults++;
+           }   */
+        
+        // Page fault
+        if (found == 0)
         {
-            // check empty frame
-            for (j = 0; j < m; j++)
+            pageFaults++;
+
+            // Check for empty frame
+            for (j = 0; j < f; j++)
             {
-                if (b[j] == -1)
+                if (frame[j] == -1)
                 {
-                    b[j] = a[i];
-                    c++;
+                    frame[j] = pages[i];
+                    time[j] = i + 1;
                     found = 1;
                     break;
                 }
             }
-
-            // no empty → optimal replace
-            if (!found)
-            {
-                max = -1;
-                p = -1;
-
-                for (j = 0; j < m; j++)
-                {
-                    for (s = i + 1; s < n; s++)
-                    {
-                        if (a[s] == b[j])
-                        {
-                            if (s > max)
-                            {
-                                max = s;
-                                p = j;
-                            }
-                            break;
-                        }
-                    }
-
-                    if (s == n)   // not used again
-                    {
-                        p = j;
-                        break;
-                    }
-                }
-
-                b[p] = a[i];
-                c++;
-            }
         }
 
-        printf("\nFrame contents: ");
-        for (k = 0; k < m; k++)
-            printf("%d ", b[k]);
+        // Replace least recently used page
+        if (found == 0)
+        {
+            least = 0;
+            for (j = 1; j < f; j++)
+            {
+                if (time[j] < time[least])
+                    least = j;
+            }
+
+            frame[least] = pages[i];
+            time[least] = i + 1;
+        }
+
+        // Display frames
+        printf("%d\t", pages[i]);
+        for (j = 0; j < f; j++)
+        {
+            if (frame[j] != -1)
+                printf("%d ", frame[j]);
+            else
+                printf("- ");
+        }
+        printf("\n");
     }
 
-    printf("\n\nNumber of page faults is: %d\n", c);
+    printf("\nTotal Page Faults = %d\n", pageFaults);
 
     return 0;
 }
-
